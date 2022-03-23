f8cb1c8cab0afe4612408ac3f279121f3d717bfb
Zonnaslyric <lovelyjhames@gmail.com>
2022-03-23 16:59:10 +0100

    updates

--- src/index.tsx
+++ src/index.tsx
@@ -1,8 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './input.css';
-import './chat.css';
-
+import './assets/css/input.css';
+import './assets/css/chat.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
